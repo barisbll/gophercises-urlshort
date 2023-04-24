c9686583b86df64d0f9c95de9ee92e3e4d684559
Baris Balli <barisbll.dev@gmail.com>
2023-04-24 09:07:10 +0200

    flags added

--- handler.go
+++ handler.go
@@ -6,28 +6,19 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
+type Yaml struct {
+	Path string `yaml:"path"`
+	URL  string `yaml:"url"`
+}
+
 func contains(m map[string]string, str string) bool {
-	for k, _ := range m {
+	for k := range m {
 		if k == str {
 			return true
 		}
 	}
 
 	return false
-}
-
-func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if contains(pathsToUrls, r.URL.Path) {
-			http.Redirect(w, r, pathsToUrls[r.URL.Path], http.StatusSeeOther)
-		}
-		fallback.ServeHTTP(w, r)
-	}
-}
-
-type Yaml struct {
-	Path string `yaml:"path"`
-	URL  string `yaml:"url"`
 }
 
 func containsYaml(v []Yaml, str string) string {
@@ -40,8 +31,17 @@ func containsYaml(v []Yaml, str string) string {
 	return ""
 }
 
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if contains(pathsToUrls, r.URL.Path) {
+			http.Redirect(w, r, pathsToUrls[r.URL.Path], http.StatusSeeOther)
+		}
+		fallback.ServeHTTP(w, r)
+	}
+}
+
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	var values []Yaml
+	values := []Yaml{}
 
 	err := yaml.Unmarshal(yml, &values)
 	if err != nil {
--- main/main.go
+++ main/main.go
@@ -1,33 +1,44 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 
 	urlshort "github.com/barisbll/gophercises-urlshort"
 )
 
-func main() {
-	mux := defaultMux()
+var pathsToUrls = map[string]string{
+	"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+	"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+}
 
-	pathsToUrls := map[string]string{
-		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
-		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
-	}
-	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
-
-	yaml := `
+var defaultYaml = `
 - path: /urlshort
   url: https://github.com/gophercises/urlshort
 - path: /urlshort-final
   url: https://github.com/gophercises/urlshort/tree/solution
 `
-	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+
+// TODO: create a serveYaml function that reads yaml files and serve them
+// if no port and yaml file provided then use the default yaml
+func main() {
+	yamlFileFlag := flag.String("yaml", "", "Location of the yaml file passed")
+	portFlag := flag.Int("port", 8080, "Port number that is serving the web server")
+	flag.Parse()
+	fmt.Printf("yamlFileFlag %s\n", *yamlFileFlag)
+
+	mux := defaultMux()
+
+	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
+
+	yamlHandler, err := urlshort.YAMLHandler([]byte(defaultYaml), mapHandler)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Starting the server on :8080")
-	http.ListenAndServe(":8080", yamlHandler)
+
+	fmt.Printf("Starting the server on :%d", *portFlag)
+	http.ListenAndServe(fmt.Sprintf(":%d", *portFlag), yamlHandler)
 }
 
 func defaultMux() *http.ServeMux {
